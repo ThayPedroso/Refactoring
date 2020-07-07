541f1a3fe4e376e78fe277327c07ebaf0cd396a2
ThayPedroso <thayla.pedroso88@gmail.com>
2020-07-07 20:05:22 -0300

    ReplaceTempWithQuery
    Substituir variável temporária por consulta.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function statement (invoice, plays) {
     }).format
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID]
+        const play = playFor(perf)
         let thisAmount = amountFor(perf, play)
 
         // soma créditos por volume
@@ -28,6 +28,10 @@ function statement (invoice, plays) {
     result += `Amount owed is ${format(totalAmount/100)}\n`
     result += `You earned ${volumeCredits} credits\n`
     return result
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
+    }
 
     function amountFor(aPerformance, play) {
         let result = 0
