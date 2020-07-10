b41406aa72d4ec694a679c64fb7e0de1dea31a7c
ThayPedroso <thayla.pedroso88@gmail.com>
2020-07-09 21:49:55 -0300

    ReplaceTotalAmount

--- index.js
+++ index.js
@@ -9,26 +9,25 @@ function statement (invoice, plays) {
     for (let perf of invoice.performances) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`
     }
-    let totalAmount = apple()
-    
-    result += `Amount owed is ${usd(totalAmount/100)}\n`
+
+    result += `Amount owed is ${usd(totalAmount())}\n`
     result += `You earned ${totalVolumeCredits()} credits\n`
     return result
 
-    function apple() {
-        let totalAmount = 0
+    function totalAmount() {
+        let result = 0
         for (let perf of invoice.performances) {
-            totalAmount += amountFor(perf)
+            result += amountFor(perf)
         }
-        return totalAmount
+        return result
     }
 
     function totalVolumeCredits(){
-        let volumeCredits = 0
+        let result = 0
         for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf)
+            result += volumeCreditsFor(perf)
         }
-        return volumeCredits
+        return result
     }
 
     function usd(aNumber) {
